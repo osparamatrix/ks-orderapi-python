4cda407825a308cf28d8415b2eb6c00e692735b5
Bhushan Patil <thebhushanp@gmail.com>
2020-09-09 23:24:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "openapi_client"
-VERSION = "1.0.7"
+VERSION = "1.0.8"
 # To install the library, run the following
 #
 # python setup.py install
