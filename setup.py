f7ce930a642d5a8bbd05fdc509b2f8b0c498ddd4
Bhushan Patil <thebhushanp@gmail.com>
2021-01-28 14:42:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "ks-api-client"
-VERSION = "1.0.17"
+VERSION = "1.0.18"
 # To install the library, run the following
 #
 # python setup.py install
