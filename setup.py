47d9a9aabbba7793bd67ec631a847cda5aa1d6ee
osparamatrix <thebhushanp@gmail.com>
2021-01-29 14:19:16 +0530

    fixed history api

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "ks-api-client"
-VERSION = "1.0.20"
+VERSION = "1.0.21"
 # To install the library, run the following
 #
 # python setup.py install
